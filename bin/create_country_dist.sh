a4afde7560478d87a4ec21e6ed00f4e01e321fd5
debat006 <kyle_debates@yahoo.com>
2017-09-14 11:52:45 -0500

    figured out how to make join work correctly. just need to awk the country code out of the resulting output.

--- bin/create_country_dist.sh
+++ bin/create_country_dist.sh
@@ -2,7 +2,7 @@
 
 directory=$1
 
-awk '{print $5}' $directory/failed_login_data.txt >> ip_test.txt|sort
+awk '{print $5}' $directory/failed_login_data.txt > ip_test.txt
 
-join -1 1 -2 1 ip_test.txt ../etc/country_IP_map.txt > country_mapped.txt
 
+join -1 1 -2 1 <(sort ip_test.txt) ../etc/country_IP_map.txt| awk '{print 4}'
