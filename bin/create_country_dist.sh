e225565bbbf8b11412f991a9fc7b715225a3cda4
Kyle DeBates (TasteDasRainbow) <31316815+TasteDasRainbow@users.noreply.github.com>
2017-09-19 20:51:11 -0500

    Added comments

--- bin/create_country_dist.sh
+++ bin/create_country_dist.sh
@@ -1,19 +1,24 @@
 #!/bin/bash
 
+#directory parameter
 directory=$1
 
+#recursively collects designated files from specified directory and concatenates them into a single file.
 grep -r "failed_login_data.txt" $directory| cat > failed_login_collective.txt
 
-
+#retrieves ip's from designated field in failed login and puts them in their own file
 awk '{print $5}' failed_login_collective.txt > ip_test.txt
 
+#merges country IP list with the sorted ips into the required format
 join -1 1 -2 1 <(sort ip_test.txt) ./etc/country_IP_map.txt > country_code.txt
 
+#retrives country code from text file to be, sorted and counted, which must occur in order, and concatenates the count into country code text
 awk '{print $2}' country_code.txt| sort | uniq -c | awk '{print "data.addRow([\x27"$2"\x27, "$1"]);"}'| cat > Country_output.txt
 
+#wraps country code text data in html to output as chart
 ./bin/wrap_contents.sh Country_output.txt ./html_components/country_dist $directory/country_dist.html
 
-
+#removes superfluous files 
 rm failed_login_collective.txt
 rm ip_test.txt
 rm country_code.txt
