0839427fa5e5aedb1a72336daa0d835946ab4223
Kyle DeBates (TasteDasRainbow) <31316815+TasteDasRainbow@users.noreply.github.com>
2017-09-19 20:42:26 -0500

    Added comments

--- bin/assemble_report.sh
+++ bin/assemble_report.sh
@@ -1,9 +1,13 @@
 #!/bin/bash 
 
+#Creating directory parameter
 directory=$1
 
+#concatenate the files from the given directories into the designated file
 cat $directory/username_dist.html hours_dist.html $directory/country_dist.html >> failed_login_summary_temp.html
 
+#Wraps html header and footer onto login summary to make html chart
 ./bin/wrap_contents.sh failed_login_summary_temp.html ./html_components/summary_plots failed_login_summary.html $directory
 
+#removes scratch file used to create chart
 rm failed_login_summary_temp.html
